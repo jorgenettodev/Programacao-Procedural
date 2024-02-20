5bb4e4ea7da2b815a3bb5d8a68a16ad156acd266
Jorge Netto <jorgenetto96@gmail.com>
2024-02-20 17:35:34 -0300

    Loop

---
+++ Listas/Lista2/Exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int contador = 1; contador <= 100000000; contador++)
+        {
+            Console.WriteLine(contador);
+        }
+    }
+}
