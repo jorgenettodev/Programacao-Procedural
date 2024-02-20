2ba9b0b0b6da3cfe003f8bb84f403ff5a1dcbe93
Jorge Netto <jorgenetto96@gmail.com>
2024-02-20 17:27:09 -0300

    lista 1 - entrada, saida e operadores logico e relaciona.

---
+++ Listas/Lista1/Exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Recebe uma idade em dias, converte em anos. Se for maior de idade, lança true, caso contrário, false.
+        Console.WriteLine("Digite sua idade em dias :");
+        int idadeEmDias = int.Parse(Console.ReadLine());
+        int idadeEmAnos = idadeEmDias / 365;
+        bool ehMaiorDeIdade = idadeEmAnos >= 18;
+        Console.WriteLine($"{idadeEmDias} equivale à {idadeEmAnos} anos, será que você é maior de idade? {ehMaiorDeIdade}");
+    }
+}
