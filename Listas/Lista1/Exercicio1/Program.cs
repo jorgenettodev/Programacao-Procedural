dc58bb95c94d9556161fd1190d5340968ea641d8
Jorge Netto <jorgenetto96@gmail.com>
2024-02-21 11:35:34 -0300

    Lista 1 - Exercicio 1

--- Listas/Lista1/Exercicio1/Program.cs
+++ Listas/Lista1/Exercicio1/Program.cs
@@ -1,14 +1,48 @@
 ﻿using System;
 
+
+/* 
+Crie um programa na linguagem C# que calcule a média de um aluno de uma escola. Seu programa deve receber as 4 notas do aluno e extrair a média das 4 notas usando a seguinte fórmula:
+
+media = (nota1 + nota2 + nota3 + nota4) / 4.0
+
+Ao final, seu programa deve exibir a mensagem: 
+“A media do aluno eh: x“ 
+
+Exemplo de entrada:
+6.2
+7.3
+3.5
+3.7
+
+Saída esperada:
+A media do aluno eh: 5.175
+
+*/
+
 class Program
 {
     static void Main(string[] args)
     {
-        // Recebe uma idade em dias, converte em anos. Se for maior de idade, lança true, caso contrário, false.
-        Console.WriteLine("Digite sua idade em dias :");
-        int idadeEmDias = int.Parse(Console.ReadLine());
-        int idadeEmAnos = idadeEmDias / 365;
-        bool ehMaiorDeIdade = idadeEmAnos >= 18;
-        Console.WriteLine($"{idadeEmDias} equivale à {idadeEmAnos} anos, será que você é maior de idade? {ehMaiorDeIdade}");
+        // input
+        Console.WriteLine("Digite suas 4 notas que deseja saber a média.");
+
+        Console.WriteLine("1a nota: ");
+        double nota1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("2a nota: ");
+        double nota2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("3a nota: ");
+        double nota3 = double.Parse(Console.ReadLine());
+        Console.WriteLine("4a nota: ");
+        double nota4 = double.Parse(Console.ReadLine());
+
+        
+
+
+        // processamento
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4.0;
+        // output
+        Console.WriteLine($"A sua media eh:  {media}");
     }
 }
--- Listas/Lista1/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
+++ Listas/Lista1/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874ebf4036020102affaf3d384f5957cf149583f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb4e4ea7da2b815a3bb5d8a68a16ad156acd266")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
