3754705292c1dca2939e9a56d131d5be35761994
jorgenettodev <jorgenetto96@gmail.com>
2024-03-07 20:31:43 -0300

    Lista 3 - Exercicio 3

---
+++ Listas/Lista3/Exercicio3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int QUANTIDADE_MAXIMA_TENTATIVAS = 3;
+        const string loginCorreto = "admin";
+        const string senhaCorreta = "123senha";
+        int i = 1;
+
+        System.Console.WriteLine("Digite seu nome de usuario e senha.");
+        do
+        {
+            System.Console.WriteLine("\nlogin: ");
+            string loginUsuario = Console.ReadLine();
+
+            System.Console.WriteLine("senha: ");
+            string senhaUsuario = Console.ReadLine();
+
+            if (loginUsuario == loginCorreto && senhaUsuario == senhaCorreta) // se acertar, sai do programa.
+            {
+                System.Console.WriteLine("\nLogin realizado com sucesso");
+                break;
+            } else if (i == QUANTIDADE_MAXIMA_TENTATIVAS) // se errar depois de 3 tentativas:
+            {
+                System.Console.WriteLine("\nSua conta foi bloqueada");
+                break;
+            } else // se errar, tente novamente e aumenta o contador
+            {
+                System.Console.WriteLine("\ntente novamente");
+                i++;
+            }
+        }
+        while (i <= QUANTIDADE_MAXIMA_TENTATIVAS);
+    }
+}
