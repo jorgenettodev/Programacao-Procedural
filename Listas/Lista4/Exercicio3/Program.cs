1cb335ac3c2feeff028c36a5576484f18dc6aabd
jorgenettodev <jorgenetto96@gmail.com>
2024-03-13 05:41:01 -0300

    Lista 4, Exercicio 3

---
+++ Listas/Lista4/Exercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // faça um programa que preencha as lateriais do tabuleiro com a letra "s";
+        Tabuleiro.InicializaTabuleiro();
+
+        int inicio = 0;
+        int final = 7;
+
+        for (int linha = inicio; linha <= final; linha++)
+        {
+            for (int coluna = inicio; coluna <= final; coluna++)
+            {
+                // Se for a linha inicial ou final, imprime
+                if (linha == inicio || linha == final)
+                {
+                    Tabuleiro.Adicionar(linha, coluna, "s");
+                }
+
+                // Se for a coluna inicial ou final, imprime
+                if (coluna == inicio || coluna == final)
+                {
+                    Tabuleiro.Adicionar(linha, coluna, "s");
+                }
+            }
+        }
+        
+        // imprime o tabuleiro final
+        Tabuleiro.ImprimeTabuleiro();
+    }
+}
