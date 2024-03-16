6a1d383b3ff0c2facb3fc0e1afa56fcf86b09f66
jorgenettodev <jorgenetto96@gmail.com>
2024-03-16 16:12:48 -0300

    Aula 9 - laços de repeticao ate o burnout

---
+++ Aulas/Aula9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
