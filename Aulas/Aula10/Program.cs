43866d1ad90f366bb3f845c01a3318bc7488c0e6
jorgenettodev <jorgenetto96@gmail.com>
2024-03-17 09:18:14 -0300

    Aula10-Exercicio5

---
+++ Aulas/Aula10/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Crie um programa que utilize laços de repetição para imprimir o seguinte padrão numérico:
+9
+98
+987
+9876
+98765
+987654
+9876543
+98765432
+987654321
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int outer = 9; outer >= 1; outer--)
+        {
+            for (int inner = 9; inner >= outer; inner--)
+            {
+                System.Console.Write(inner);
+            }
+            System.Console.WriteLine();
+        }
+
+    }
+}
