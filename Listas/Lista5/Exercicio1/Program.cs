ba06027d9d5d83be94741d1a99c6e4eca60395be
jorgenettodev <jorgenetto96@gmail.com>
2024-03-18 19:38:05 -0300

    Lista 5, exercicio 1

---
+++ Listas/Lista5/Exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int regressivo = 200;
+        for (int progressivo = 0; progressivo <= 100; progressivo +=2 )
+        {
+            System.Console.WriteLine($"{progressivo} - {regressivo}");
+            regressivo -= 4;
+        }
+    }
+}
