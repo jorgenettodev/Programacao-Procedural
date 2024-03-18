014e176fe3a1eff891ff905748060d630e56968c
jorgenettodev <jorgenetto96@gmail.com>
2024-03-18 20:11:56 -0300

    Lista 5 - Exercicio 5

---
+++ Listas/Lista5/Exercicio5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tabuleiro.InicializaTabuleiro();
+        for (int contadorExterno = 0; contadorExterno <= 7; contadorExterno++) 
+        // obs: coloquei os nomes contadorExterno e contadorInterno para fixar este conteúdo e entender melhor o funcionamento de laços aninhados.
+        {
+            for(int contadorInterno = 0; contadorInterno <= 7; contadorInterno++)
+            {
+                if (contadorExterno % 2 != 0)
+                {
+                    continue; // se a linha for impar, pule linha;
+                }
+                Tabuleiro.Adicionar(contadorExterno, contadorInterno, "C");
+            }
+
+        }
+        Tabuleiro.ImprimeTabuleiro();
+    }
+}
