d8eb7fc842113262a3284ea25b09b151f52bfbb9
jorgenettodev <jorgenetto96@gmail.com>
2024-03-18 19:41:48 -0300

    Lista 5 - exercicio2

---
+++ Listas/Lista5/Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int multiplicando = 10; multiplicando >= 1; multiplicando--)
+        {
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                int produto = multiplicando * multiplicador;
+                System.Console.WriteLine($"{multiplicando} * {multiplicador} = {produto}");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
