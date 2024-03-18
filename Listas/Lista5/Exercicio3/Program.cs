70376e10c3eff3f303c89815490482c9151f5952
jorgenettodev <jorgenetto96@gmail.com>
2024-03-18 19:45:19 -0300

    Lista 5 - exercicio3

---
+++ Listas/Lista5/Exercicio3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int alturaRetangulo = int.Parse(Console.ReadLine());
+        int baseRetangulo = int.Parse(Console.ReadLine());
+
+        for (int linhas = 0; linhas < alturaRetangulo; linhas++)
+        {
+            for (int colunas = 0; colunas < baseRetangulo; colunas++)
+            {
+                Console.Write("*"); // imprime um "*" por coluna;
+            }
+            System.Console.WriteLine(); // pula linha
+        }
+    }
+}
