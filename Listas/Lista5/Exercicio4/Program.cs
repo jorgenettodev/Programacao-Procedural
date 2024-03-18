92abb41eace563e61cbbc4f62f380081af869cda
jorgenettodev <jorgenetto96@gmail.com>
2024-03-18 19:49:58 -0300

    Lista 5 - Exercicio4

---
+++ Listas/Lista5/Exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int somatorioNumerosImpares = 0;
+        do
+        {
+            int inputNumero = int.Parse(Console.ReadLine());
+
+            if (inputNumero < 0 ) // se numero for negativo, pare
+            {
+                break;
+            }
+            if (inputNumero % 2 != 0)
+            {
+                somatorioNumerosImpares += inputNumero;
+            }
+
+        }
+        while (true);
+        System.Console.WriteLine($"O somatorio de impares eh: {somatorioNumerosImpares}");
+    }
+}
