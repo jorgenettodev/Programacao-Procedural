19f06c3e9969d123346a547937fe0fa8dd8428e4
jorgenettodev <jorgenetto96@gmail.com>
2024-03-26 19:28:42 -0300

    L6E4

---
+++ Listas/Lista6/Exercicio4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_MATRIZ = 3;
+        int[,] matriz = new int[TAMANHO_MATRIZ, TAMANHO_MATRIZ];
+
+        for (int linha = 0; linha < TAMANHO_MATRIZ; linha++)
+        {
+            for (int coluna = 0; coluna < TAMANHO_MATRIZ; coluna++)
+            {
+                int inputNumero = int.Parse(Console.ReadLine());
+
+                if (linha + coluna == TAMANHO_MATRIZ - 1)
+                {
+                    matriz[linha, coluna] = inputNumero * 2;
+                    continue;
+                }
+                
+                matriz[linha, coluna] = inputNumero;
+            }
+        }
+
+        for (int linha = 0; linha < TAMANHO_MATRIZ; linha++)
+        {
+            for (int coluna = 0; coluna < TAMANHO_MATRIZ; coluna++)
+            {
+                Console.Write($"{matriz[linha, coluna]}|");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
