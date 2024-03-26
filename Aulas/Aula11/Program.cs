af1a72d83f831fc0d4796b7b9367aceece7797ed
jorgenettodev <jorgenetto96@gmail.com>
2024-03-26 07:00:33 -0300

    Matriz

---
+++ Aulas/Aula11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] idades = new int[4];
+
+        for (int contador = 0; contador < idades.Length; contador++)
+        {
+            System.Console.WriteLine("Escreva sua idade");
+            // idades[contador] = Math.
+        }
+    }
+}
