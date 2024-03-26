79216769d6d1b2fb7eadaff29f99f30dcb548a3b
jorgenettodev <jorgenetto96@gmail.com>
2024-03-26 18:44:03 -0300

    l6e2

---
+++ Listas/Lista6/Exercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int QUANTIDADE_PESSOAS = 10;
+        string[] vetorPessoas = new string[QUANTIDADE_PESSOAS];
+
+        for (int indicePessoa = 0; indicePessoa < QUANTIDADE_PESSOAS; indicePessoa++)
+        {
+            vetorPessoas[indicePessoa] = Console.ReadLine();
+        }
+
+        for (int indicePessoa = QUANTIDADE_PESSOAS - 1; indicePessoa >= 0; indicePessoa--)
+        {
+            if (indicePessoa % 2 == 0)
+            {
+                Console.WriteLine(vetorPessoas[indicePessoa]);
+            }
+        }
+    }
+}
