a103033fa9d688136d4c5ec47c41510198df5e56
jorgenettodev <jorgenetto96@gmail.com>
2024-03-26 19:20:16 -0300

    L6E3

---
+++ Listas/Lista6/Exercicio3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // matriz
+        const int TAMANHO_MATRIZ = 3;
+        int[,] matriz = new int[TAMANHO_MATRIZ, TAMANHO_MATRIZ];
+        int somatorioDiagonalPrincipal = 0;
+
+        for (int linha = 0; linha < TAMANHO_MATRIZ; linha++)
+        {
+            for (int coluna = 0; coluna < TAMANHO_MATRIZ; coluna++)
+            {
+                matriz[linha, coluna] = int.Parse(Console.ReadLine());
+                if (linha == coluna)
+                {
+                    somatorioDiagonalPrincipal += matriz[linha, coluna];
+                }
+            }
+        }
+        System.Console.WriteLine($"{somatorioDiagonalPrincipal}");
+    }
+}
