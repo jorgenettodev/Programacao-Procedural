4942250d6f3f2192ee16b058948cbf817848f451
jorgenettodev <jorgenetto96@gmail.com>
2024-04-12 04:26:31 -0300

    Lista 8 - Exercicio 1 - OK

---
+++ Listas/Lista8/Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza1;
+
+        pizza1.id = Convert.ToInt32(Console.ReadLine());
+        pizza1.sabor = Console.ReadLine();
+        pizza1.preco = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine(pizza1.id);
+        System.Console.WriteLine(pizza1.sabor);
+        System.Console.WriteLine(pizza1.preco);
+    }
+}
