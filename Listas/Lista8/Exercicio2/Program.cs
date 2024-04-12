bbb94721cf67516f246b9d194b29ec985c1191a4
jorgenettodev <jorgenetto96@gmail.com>
2024-04-12 05:00:01 -0300

    Lista 8 - Exercicio 2

---
+++ Listas/Lista8/Exercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto[] produtos = new Produto[4];  // cria uma variável "produtos" do tipo Produto, que recebe um array de tamanho 4;
+
+        // cria vitrine
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            produtos[i].id = Convert.ToInt32(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+            produtos[i].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+        }
+
+        // mostra vitrine com produtos disponíveis
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            if (produtos[i].disponivelEmEstoque)
+            {
+                System.Console.WriteLine(produtos[i].nome);
+                System.Console.WriteLine($"{produtos[i].preco:F2}");
+            }
+        }
+    }
+}
